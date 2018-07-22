c62d7cdc2aa8e1ed91d2c165323dc032bfa07f91
intxparts <intxparts@gmail.com>
2018-07-22 11:07:42 -0400

    added BoolField unittests

--- tests.py
+++ tests.py
@@ -9,6 +9,58 @@ class is_built_in_name_tests(unittest.TestCase):
         for t in [True, [], {}, 1, -2.1, None, '__data', '__data_', 'data__', '_data__']:
             self.assertFalse(psm._is_builtin_name(t))
 
+class BoolField_tests(unittest.TestCase):
+    def test_validation_required_enabled(self):
+        class Model(psm.SchemaModel):
+            field = psm.BoolField(required=True)
+        
+        m1 = Model()
+        missing_result, missing_errors = m1.validate()
+        self.assertFalse(missing_result)
+        self.assertEqual(1, len(missing_errors))
+
+        m2 = Model(field=True)
+        result, errors = m2.validate()
+        self.assertTrue(result)
+        self.assertEqual(0, len(errors))
+
+    def test_validation_required_disabled(self):
+        class Model(psm.SchemaModel):
+            field = psm.BoolField(required=False)
+
+        m1 = Model()
+        missing_result, missing_errors = m1.validate()
+        self.assertTrue(missing_result)
+        self.assertEqual(0, len(missing_errors))
+
+    def test_validation_nullable_bool(self):
+        class Model(psm.SchemaModel):
+            field = psm.BoolField(nullable=True)
+        
+        m1 = Model(field=None)
+        null_result, null_errors = m1.validate()
+        self.assertTrue(null_result)
+        self.assertEqual(0, len(null_errors))
+
+        m2 = Model(field=False)
+        non_null_result, non_null_errors = m2.validate()
+        self.assertTrue(non_null_result)
+        self.assertEqual(0, len(non_null_errors))
+
+    def test_validation_nonnullable_bool(self):
+        class Model(psm.SchemaModel):
+            field = psm.BoolField(nullable=False)
+        
+        m1 = Model(field=None)
+        null_result, null_errors = m1.validate()
+        self.assertFalse(null_result)
+        self.assertEqual(1, len(null_errors))
+
+        m2 = Model(field=True)
+        nonnull_result, nonnull_errors = m2.validate()
+        self.assertTrue(nonnull_result)
+        self.assertEqual(0, len(nonnull_errors))
+
 
 
 if __name__ == '__main__':
